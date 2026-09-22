0f157cbf664e3da2686249dca8f8aaaafbe92b19
ONNELLAB <akinstun21@gmail.com>
2026-09-22 09:34:16 +0900

    feat(connect): route provider launcher to Lyria

--- scripts/install_youtube_connect.py
+++ scripts/install_youtube_connect.py
@@ -17,8 +17,8 @@ APP_ID = 'com.onnellab.content-engine.youtube-connect'
 APP_NAME = 'ONNELLAB YouTube Connect.app'
 SCHEME = 'onnellab-content'
 YOUTUBE_URLS = (SCHEME+'://youtube/connect', SCHEME+'://youtube/status')
-SUNO_URLS = (SCHEME+'://suno/connect', SCHEME+'://suno/status')
-URLS = YOUTUBE_URLS + SUNO_URLS
+LYRIA_URLS = (SCHEME+'://lyria/connect', SCHEME+'://lyria/status')
+URLS = YOUTUBE_URLS + LYRIA_URLS
 LSREGISTER = '/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister'
 
 
@@ -29,16 +29,16 @@ def launch_command(python, script):
 
 def applescript_source(python, script):
     youtube_command=launch_command(python,script).replace('\\','\\\\').replace('"','\\"')
-    suno_script=Path(script).with_name('suno_api_connect.py')
-    suno_command=launch_command(python,suno_script).replace('\\','\\\\').replace('"','\\"')
+    lyria_script=Path(script).with_name('lyria_connect.py')
+    lyria_command=launch_command(python,lyria_script).replace('\\','\\\\').replace('"','\\"')
     return ('on launchYouTube()\n  do shell script "'+youtube_command+'"\nend launchYouTube\n'
-            'on launchSuno()\n  do shell script "'+suno_command+'"\nend launchSuno\n'
+            'on launchLyria()\n  do shell script "'+lyria_command+'"\nend launchLyria\n'
             'on run\n  my launchYouTube()\nend run\n'
             'on open location targetURL\n'
             '  if targetURL is "'+YOUTUBE_URLS[0]+'" or targetURL is "'+YOUTUBE_URLS[1]+'" then\n'
             '    my launchYouTube()\n'
-            '  else if targetURL is "'+SUNO_URLS[0]+'" or targetURL is "'+SUNO_URLS[1]+'" then\n'
-            '    my launchSuno()\n'
+            '  else if targetURL is "'+LYRIA_URLS[0]+'" or targetURL is "'+LYRIA_URLS[1]+'" then\n'
+            '    my launchLyria()\n'
             '  end if\nend open location\n')
 
 
@@ -48,8 +48,8 @@ def install(*, applications=None, dry_run=False):
     target=applications/APP_NAME
     python=Path(sys.executable).resolve()
     script=ROOT/'scripts/short_video_connect.py'
-    suno_script=ROOT/'scripts/suno_api_connect.py'
-    if not script.is_file() or not suno_script.is_file() or not python.is_file(): raise RuntimeError('launcher_source_missing')
+    lyria_script=ROOT/'scripts/lyria_connect.py'
+    if not script.is_file() or not lyria_script.is_file() or not python.is_file(): raise RuntimeError('launcher_source_missing')
     if target.exists():
         try:
             if target.is_symlink(): raise ValueError()
@@ -70,8 +70,8 @@ def install(*, applications=None, dry_run=False):
         info=build/'Contents/Info.plist'
         with info.open('rb') as stream: config=plistlib.load(stream)
         config.update(CFBundleIdentifier=APP_ID, CFBundleName='ONNELLAB Provider Connect',
-            CFBundleDisplayName='ONNELLAB Provider Connect',CFBundleShortVersionString='1.1',
-            CFBundleVersion='2',LSUIElement=True,
+            CFBundleDisplayName='ONNELLAB Provider Connect',CFBundleShortVersionString='1.2',
+            CFBundleVersion='3',LSUIElement=True,
             CFBundleURLTypes=[{'CFBundleURLName':APP_ID,'CFBundleURLSchemes':[SCHEME]}])
         with info.open('wb') as stream: plistlib.dump(config,stream)
         signed=subprocess.run(['/usr/bin/codesign','--force','--sign','-',str(build)],
